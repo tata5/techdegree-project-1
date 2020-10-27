a6c1f19df4341b699a259d4f7b9579a4dc81f0c4
Ty <tyreemack09@gmail.com>
2020-10-26 17:57:24 -0700

    Update
    Uodated quote charcters.

--- js/script.js
+++ js/script.js
@@ -3,88 +3,86 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance:
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /***
- * `quotes` array
+ The object, quotes, displays the array properties of famous black authors. These citations come directly from sources
+ of the author's work throughout the years.
 ***/
 const quotes = [
   {
-    author: "W.E.B. Du Bois",
-    quotation: "Daily the Negro is coming more and more to look upon law and justice, not as protecting safeguards, but as sources of humiliation and oppression. The laws are made by men who have little interest in him; they are executed by men who have absolutely no motive for treating the black people with courtesy or consideration; and, finally, the accused law-breaker is tried, not by his peers, but too often by men who would rather punish ten innocent Negroes than let one guilty one escape.",
+    citation: "W.E.B. Du Bois",
+    quote: "Daily the Negro is coming more and more to look upon law and justice, not as protecting safeguards, but as sources of humiliation and oppression. The laws are made by men who have little interest in him; they are executed by men who have absolutely no motive for treating the black people with courtesy or consideration; and, finally, the accused law-breaker is tried, not by his peers, but too often by men who would rather punish ten innocent Negroes than let one guilty one escape.",
     year: 1903,
-    book: "The Souls of Black Folks"
+    source: "The Souls of Black Folks"
   },
   {
-    author: "Toni Morrison",
-    quotation: "Freeing yourself was one thing, claiming ownership of that freed self was another.",
+    citation: "Toni Morrison",
+    quote: "Freeing yourself was one thing, claiming ownership of that freed self was another.",
     year: 1987,
-    book: "Beloved"
+    source: "Beloved"
   },
   {
-    author: "Maya Angelou",
-    quotation: "Words mean more than what is set down on paper. It takes the human voice to infuse them with shades of deeper meaning.",
+    citation: "Maya Angelou",
+    quote: "Words mean more than what is set down on paper. It takes the human voice to infuse them with shades of deeper meaning.",
     year: 1969,
-    book: "I Know Why the Caged Bird Sing"
+    source: "I Know Why the Caged Bird Sing"
   },
   {
-    author: "Alice Walker",
-    quotation: "Keep in mind always the present you are constructing. It should be the future you want.",
+    citation: "Alice Walker",
+    quote: "Keep in mind always the present you are constructing. It should be the future you want.",
     year: 1990,
-    book: "The Temple of My Familiar"
+    source: "The Temple of My Familiar"
   },
   {
-    author: "Booker T. Washington",
-    quotation: "Those who are happiest are those who do the most for others.",
+    citation: "Booker T. Washington",
+    quote: "Those who are happiest are those who do the most for others.",
     year: 1901,
-    book: "Up from Slavery"
+    source: "Up from Slavery"
   },
   {
-    author: "Ta-Nehisi Coates",
-    quotation: "I would not have you descend into your own dream. I would have you be a conscious citizen of this terrible and beautiful world.",
+    citation: "Ta-Nehisi Coates",
+    quote: "I would not have you descend into your own dream. I would have you be a conscious citizen of this terrible and beautiful world.",
     year: 2015,
-    book: "Between the World and Me"
-  },
+    source: "Between the World and Me"
+  }
 ];
 
 /***
- * `The objective of creating getRandomQuote`function is to generate a random number from the quote object.
+ The objective of creating getRandomQuotefunction is to generate a random number from the quote object.
 ***/
 function getRandomQuote () {
   const randomNumber = Math.floor(Math.random() * quotes.length);
-  console.log(quotes);
   return quotes[randomNumber];
 
 };
 getRandomQuote();
 
 /***
- * `printQuote` function
+ The printQuote function is added to the code in order to drawn out the random quotes objects
+ from the quote array. So when the printQuote is clicked, a string of HTML and quote properties
+ are displayed.
 ***/
-function printQuote () {
-  var randomQuote = getRandomQuote(quotes);
-  var showQuote = '<p class = "quotation">' + randomQuote.quotation + '</p>';
-      showQuote += '<p class = "book">' + randomQuote.book;
 
-  if (randomQuote.author){
-    showQuote = '<span class = "author">' + randomQuote.author + '</span>';
+function printQuote () {
+  let randomQuote = getRandomQuote(quotes);
+  let showQuote = '<p class = "quote">' + randomQuote.quote + '</p>';
+      showQuote += '<p class = "source">' + randomQuote.source;
+
+  if (randomQuote.citation){
+    showQuote += '<span class = "citation">' + randomQuote.citation + '</span>';
   };
   if (randomQuote.year){
-    showQuote = '<span class = "year">' + randomQuote.year + '</span>';
+    showQuote += '<span class = "year">' + randomQuote.year + '</span>';
   };
 
-    showQuote = '</p>';
+    showQuote += '</p>';
 
     document.getElementById('quote-box').innerHTML = showQuote;
 };
 console.log(printQuote());
 
 /***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
+ This is the event listener. When the for the print quote button is clicked, it will load
+ the quotes.
 ***/
-/***
+
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
-***/
